22a54d58d74e9f3c8df3bd59912add69028a7f81
Tobias Hunger <tobias.hunger@gmail.com>
2021-04-29 15:07:27 +0200

    Move database code out of the repository and into a backend module
    Maybe I should turn the backend into a struct holding the Db itself? Not
    sure: The API of the backend is pretty backend specific anyway!

--- gng_repository/src/repository.rs
+++ gng_repository/src/repository.rs
@@ -3,9 +3,8 @@
 
 //! A object representing a `Repository`
 
+mod backend;
 mod definitions;
-
-use definitions::RepositoryInternalData;
 
 // ----------------------------------------------------------------------
 // - Repository:
@@ -17,7 +16,9 @@ pub trait Repository {
     ///
     /// # Errors
     /// Any of the crate's `Error`s can be returned from here.
-    fn version(&self) -> crate::Result<u32>;
+    fn schema_version(&self) -> crate::Result<u32>;
+
+    // Repository management:
 
     /// Add a new repository
     ///
@@ -62,182 +63,26 @@ pub(crate) struct RepositoryImpl {
 }
 
 impl RepositoryImpl {
-    const MAGIC: &'static str = "\"1e925d91-3294-4676-add6-917376d89d58\"";
-    const MAGIC_KEY: &'static str = "magic";
-
-    const VERSION: u32 = 1;
-    const VERSION_KEY: &'static str = "schema_version";
-
-    const REPOSITORIES_TREE: &'static str = "repositories";
-    const PACKETS_TREE: &'static str = "packets";
-    const CONTENTS_TREE: &'static str = "contents";
-
     #[tracing::instrument(level = "trace")]
     pub(crate) fn new(path: &std::path::Path) -> crate::Result<Self> {
         let config = sled::Config::default().path(path.to_owned());
 
-        let mut result = Self {
-            db: config.open()?,
-            repositories: Vec::new(),
-        };
-
-        match result.db.get(Self::MAGIC_KEY)? {
-            Some(v) if v == Self::MAGIC => result.db_validate()?,
-            Some(_) => return Err(crate::Error::WrongMagic),
-            None => result.db_setup()?,
-        };
-
-        result.repositories = result.read_repositories()?;
-
-        Ok(result)
+        Self::new_from_db(config.open()?)
     }
 
-    #[tracing::instrument(level = "trace", skip(self))]
-    fn read_repositories(&mut self) -> crate::Result<Vec<crate::RepositoryData>> {
-        let tree = self.db.open_tree(Self::REPOSITORIES_TREE)?;
+    fn new_from_db(db: sled::Db) -> crate::Result<Self> {
+        backend::open_db(&db)?;
+        let repositories = backend::read_repositories(&db)?;
 
-        let (data, id_map) = {
-            let mut data = Vec::with_capacity(tree.len());
-            let mut id_map = std::collections::HashMap::with_capacity(tree.len());
-
-            for tree_result in tree.iter() {
-                match tree_result {
-                    Err(e) => return Err(e.into()),
-                    Ok((k, v)) => {
-                        let name: gng_shared::Name = serde_json::from_slice(&k[..])
-                            .map_err(|_| crate::Error::WrongSchema)?;
-                        let rid: RepositoryInternalData = serde_json::from_reader(&v[..])
-                            .map_err(|_| crate::Error::WrongSchema)?;
-
-                        id_map.insert(rid.uuid, name.clone());
-
-                        data.push((name, rid));
-                    }
-                }
-            }
-            (data, id_map)
-        };
-
-        let mut result = data
-            .iter()
-            .map(|(n, d)| -> crate::Result<crate::RepositoryData> {
-                let dependencies = gng_shared::Names::from(
-                    d.dependencies
-                        .iter()
-                        .map(|u| {
-                            let name = id_map.get(u).ok_or_else(|| {
-                                crate::Error::UnknownRepositoryDependency(u.to_string(), n.clone())
-                            })?;
-                            Ok(name.clone())
-                        })
-                        .collect::<crate::Result<Vec<gng_shared::Name>>>()?,
-                );
-
-                Ok(crate::RepositoryData {
-                    name: n.clone(),
-                    uuid: d.uuid,
-                    priority: d.priority,
-                    pull_url: d.pull_url.clone(),
-                    packet_base_url: d.packet_base_url.clone(),
-                    sources_base_directory: d.sources_base_directory.clone(),
-                    dependencies,
-                })
-            })
-            .collect::<crate::Result<Vec<crate::RepositoryData>>>()?;
-
-        result.sort();
-
-        Ok(result)
-    }
-
-    #[tracing::instrument(level = "trace", skip(self))]
-    fn write_repositories(&mut self) -> crate::Result<()> {
-        let repositories = &self.repositories[..];
-
-        let tree = self.db.open_tree(Self::REPOSITORIES_TREE)?;
-
-        let name_map: std::collections::HashMap<gng_shared::Name, crate::Uuid> = repositories
-            .iter()
-            .map(|r| (r.name.clone(), r.uuid))
-            .collect();
-
-        let batch = {
-            let mut batch = sled::Batch::default();
-
-            for r in repositories {
-                batch.insert(
-                    serde_json::to_vec(&r.name).expect("names must be convertible to JSON"),
-                    serde_json::to_vec(&definitions::RepositoryInternalData {
-                        uuid: r.uuid,
-                        priority: r.priority,
-                        pull_url: r.pull_url.clone(),
-                        packet_base_url: r.packet_base_url.clone(),
-                        sources_base_directory: r.sources_base_directory.clone(),
-                        dependencies: r
-                            .dependencies
-                            .into_iter()
-                            .map(|n| {
-                                name_map.get(n).cloned().ok_or_else(|| {
-                                    crate::Error::UnknownRepositoryDependency(
-                                        n.to_string(),
-                                        r.name.clone(),
-                                    )
-                                })
-                            })
-                            .collect::<crate::Result<Vec<crate::Uuid>>>()?,
-                    })
-                    .expect("Data structure must be convertible to JSON"),
-                )
-            }
-
-            batch
-        };
-
-        tree.apply_batch(batch)?;
-
-        tree.flush()?;
-
-        Ok(())
-    }
-
-    #[tracing::instrument(level = "trace", skip(self))]
-    fn db_setup(&mut self) -> crate::Result<()> {
-        self.db.insert(
-            Self::VERSION_KEY,
-            &serde_json::to_vec(&Self::VERSION).map_err(|_| crate::Error::WrongSchema)?[..],
-        )?;
-
-        let _repository_tree = self.db.open_tree(Self::REPOSITORIES_TREE)?;
-        let _packages_tree = self.db.open_tree(Self::PACKETS_TREE)?;
-        let _contents_tree = self.db.open_tree(Self::CONTENTS_TREE)?;
-
-        self.db.insert(Self::MAGIC_KEY, Self::MAGIC)?;
-
-        self.db.flush()?;
-        Ok(())
-    }
-
-    #[tracing::instrument(level = "trace", skip(self))]
-    fn db_validate(&mut self) -> crate::Result<()> {
-        match self.version()? {
-            v if v < Self::VERSION => Err(crate::Error::WrongSchema),
-            v if v == Self::VERSION => Ok(()),
-            _ => Err(crate::Error::WrongSchema),
-        }
+        Ok(Self { db, repositories })
     }
 }
 
 impl Repository for RepositoryImpl {
-    #[tracing::instrument(level = "trace", skip(self))]
-    fn version(&self) -> crate::Result<u32> {
-        let version = self
-            .db
-            .get(Self::VERSION_KEY)?
-            .expect("Version must be set!");
-        serde_json::from_slice(&version[..]).map_err(|_| crate::Error::WrongSchema)
+    fn schema_version(&self) -> crate::Result<u32> {
+        backend::schema_version(&self.db)
     }
 
-    #[tracing::instrument(level = "trace", skip(self))]
     fn list_repositories(&self) -> crate::Result<Vec<crate::RepositoryData>> {
         Ok(self.repositories.clone())
     }
@@ -266,15 +111,17 @@ impl Repository for RepositoryImpl {
         self.repositories.push(repository_data);
         self.repositories.sort();
 
-        self.write_repositories()
+        backend::write_repositories(&self.db, &self.repositories)
     }
 
     #[tracing::instrument(level = "trace", skip(self))]
     fn remove_repository(&mut self, name: &gng_shared::Name) -> crate::Result<()> {
         let mut to_remove = self.repositories.len();
+        let mut uuid = None;
         for (idx, r) in self.repositories.iter().enumerate() {
             if &r.name == name {
                 to_remove = idx;
+                uuid = Some(r.uuid);
             } else if r.dependencies.contains(name) {
                 return Err(crate::Error::RepositoryInUse {
                     used_repository: name.clone(),
@@ -283,53 +130,37 @@ impl Repository for RepositoryImpl {
             }
         }
 
-        if to_remove == self.repositories.len() {
+        if let Some(uuid) = uuid {
+            assert!(to_remove != self.repositories.len());
+            self.repositories.remove(to_remove);
+
+            backend::remove_repository(&self.db, &name.to_string(), &uuid)
+        } else {
             return Err(crate::Error::UnknownRepository(name.clone()));
         }
-
-        self.repositories.remove(to_remove);
-
-        let tree = self.db.open_tree(Self::REPOSITORIES_TREE)?;
-        tree.remove(&serde_json::to_vec(&name).expect("Names can be serialized!")[..])?;
-
-        tree.flush()?;
-
-        Ok(())
     }
 
-    #[tracing::instrument(level = "trace", skip(self))]
     fn fsck(&self) -> crate::Result<bool> {
         Ok(true)
     }
 
-    #[tracing::instrument(level = "trace", skip(self))]
     fn dump_metadata(&mut self) -> crate::Result<()> {
-        println!("Metadata:");
-        for data in self.db.iter() {
-            match data {
-                Err(e) => println!("Error: {}", e),
-                Ok((k, v)) => {
-                    let key = std::string::String::from_utf8((&k[..]).to_vec())
-                        .unwrap_or(format!("{:?}", k));
-                    let value = if v.is_ascii() {
-                        std::string::String::from_utf8((&v[..]).to_vec())
-                            .expect("I though this is ASCII?!")
-                    } else {
-                        format!("{:?}", v)
-                    };
-                    println!("    \"{}\" => {}.", key, value,)
-                }
-            }
-        }
+        backend::dump_metadata(&self.db)
+    }
+}
 
-        println!("\nKnown trees:");
-        for n in &self.db.tree_names() {
-            let n = std::string::String::from_utf8((&n[..]).to_vec()).unwrap_or(format!("{:?}", n));
-            if n.starts_with("__") {
-                continue;
-            }
-            println!("    \"{}\"", &n)
-        }
-        Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_repository() -> RepositoryImpl {
+        let config = sled::Config::default().temporary(true);
+        RepositoryImpl::new_from_db(config.open().expect("Temporary DB should have been valid!"))
+            .expect("DB should have been created!")
+    }
+
+    #[test]
+    fn test_repository_setup() {
+        let _repo = create_repository();
     }
 }
---
+++ gng_repository/src/repository/backend.rs
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2020 Tobias Hunger <tobias.hunger@gmail.com>
+
+//! A backend database for a `Repository`
+
+use super::definitions::RepositoryInternalData;
+use crate::RepositoryData;
+
+// ----------------------------------------------------------------------
+// - Constants:
+// ----------------------------------------------------------------------
+
+const MAGIC: &'static str = "\"1e925d91-3294-4676-add6-917376d89d58\"";
+const MAGIC_KEY: &'static str = "magic";
+
+const VERSION: u32 = 1;
+const VERSION_KEY: &'static str = "schema_version";
+
+const REPOSITORIES_TREE: &'static str = "repositories";
+const PACKETS_TREE: &'static str = "packets";
+const CONTENTS_TREE: &'static str = "contents";
+
+const PACKET_REPO_TREE_PREFIX: &'static str = "packets_";
+
+// ----------------------------------------------------------------------
+// - Helpers:
+// ----------------------------------------------------------------------
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn open_db(db: &sled::Db) -> crate::Result<()> {
+    if db.is_empty() {
+        setup(db)?
+    };
+
+    match db.get(MAGIC_KEY)? {
+        Some(v) if v == MAGIC => validate(db),
+        _ => Err(crate::Error::WrongMagic),
+    }
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn schema_version(db: &sled::Db) -> crate::Result<u32> {
+    let version = db.get(VERSION_KEY)?.expect("Version must be set!");
+    serde_json::from_slice(&version[..]).map_err(|_| crate::Error::WrongSchema)
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn setup(db: &sled::Db) -> crate::Result<()> {
+    db.insert(
+        VERSION_KEY,
+        &serde_json::to_vec(&VERSION).map_err(|_| crate::Error::WrongSchema)?[..],
+    )?;
+
+    let _repository_tree = db.open_tree(REPOSITORIES_TREE)?;
+    let _packages_tree = db.open_tree(PACKETS_TREE)?;
+    let _contents_tree = db.open_tree(CONTENTS_TREE)?;
+
+    db.insert(MAGIC_KEY, MAGIC)?;
+
+    db.flush()?;
+    Ok(())
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn validate(db: &sled::Db) -> crate::Result<()> {
+    match schema_version(db)? {
+        v if v < VERSION => Err(crate::Error::WrongSchema),
+        v if v == VERSION => Ok(()),
+        _ => Err(crate::Error::WrongSchema),
+    }
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn read_repositories(db: &sled::Db) -> crate::Result<Vec<crate::RepositoryData>> {
+    let tree = db.open_tree(REPOSITORIES_TREE)?;
+
+    let (data, id_map) = {
+        let mut data = Vec::with_capacity(tree.len());
+        let mut id_map = std::collections::HashMap::with_capacity(tree.len());
+
+        for tree_result in tree.iter() {
+            match tree_result {
+                Err(e) => return Err(e.into()),
+                Ok((k, v)) => {
+                    let name: gng_shared::Name =
+                        serde_json::from_slice(&k[..]).map_err(|_| crate::Error::WrongSchema)?;
+                    let rid: RepositoryInternalData =
+                        serde_json::from_reader(&v[..]).map_err(|_| crate::Error::WrongSchema)?;
+
+                    id_map.insert(rid.uuid, name.clone());
+
+                    data.push((name, rid));
+                }
+            }
+        }
+        (data, id_map)
+    };
+
+    let mut result = data
+        .iter()
+        .map(|(n, d)| -> crate::Result<crate::RepositoryData> {
+            let dependencies = gng_shared::Names::from(
+                d.dependencies
+                    .iter()
+                    .map(|u| {
+                        let name = id_map.get(u).ok_or_else(|| {
+                            crate::Error::UnknownRepositoryDependency(u.to_string(), n.clone())
+                        })?;
+                        Ok(name.clone())
+                    })
+                    .collect::<crate::Result<Vec<gng_shared::Name>>>()?,
+            );
+
+            Ok(crate::RepositoryData {
+                name: n.clone(),
+                uuid: d.uuid,
+                priority: d.priority,
+                pull_url: d.pull_url.clone(),
+                packet_base_url: d.packet_base_url.clone(),
+                sources_base_directory: d.sources_base_directory.clone(),
+                dependencies,
+            })
+        })
+        .collect::<crate::Result<Vec<crate::RepositoryData>>>()?;
+
+    result.sort();
+
+    Ok(result)
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn write_repositories(db: &sled::Db, repositories: &[RepositoryData]) -> crate::Result<()> {
+    let tree = db.open_tree(REPOSITORIES_TREE)?;
+
+    let name_map: std::collections::HashMap<gng_shared::Name, crate::Uuid> = repositories
+        .iter()
+        .map(|r| (r.name.clone(), r.uuid))
+        .collect();
+
+    let batch = {
+        let mut batch = sled::Batch::default();
+
+        for r in repositories {
+            batch.insert(
+                serde_json::to_vec(&r.name).expect("names must be convertible to JSON"),
+                serde_json::to_vec(&RepositoryInternalData {
+                    uuid: r.uuid,
+                    priority: r.priority,
+                    pull_url: r.pull_url.clone(),
+                    packet_base_url: r.packet_base_url.clone(),
+                    sources_base_directory: r.sources_base_directory.clone(),
+                    dependencies: r
+                        .dependencies
+                        .into_iter()
+                        .map(|n| {
+                            name_map.get(n).cloned().ok_or_else(|| {
+                                crate::Error::UnknownRepositoryDependency(
+                                    n.to_string(),
+                                    r.name.clone(),
+                                )
+                            })
+                        })
+                        .collect::<crate::Result<Vec<crate::Uuid>>>()?,
+                })
+                .expect("Data structure must be convertible to JSON"),
+            )
+        }
+
+        batch
+    };
+
+    tree.apply_batch(batch)?;
+    tree.flush()?;
+
+    Ok(())
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn remove_repository(db: &sled::Db, name: &str, uuid: &crate::Uuid) -> crate::Result<()> {
+    let tree = db.open_tree(REPOSITORIES_TREE)?;
+
+    tree.remove(&serde_json::to_vec(&name).expect("Names can be serialized!")[..])?;
+    tree.flush()?;
+
+    let repository_packets_tree = format!("{}{}", PACKET_REPO_TREE_PREFIX, uuid);
+    db.drop_tree(&repository_packets_tree[..])
+        .map(|_| ())
+        .map_err(|e| crate::Error::Backend(e))
+}
+
+#[tracing::instrument(level = "trace", skip(db))]
+pub fn dump_metadata(db: &sled::Db) -> crate::Result<()> {
+    println!("Metadata:");
+    for data in db.iter() {
+        match data {
+            Err(e) => println!("Error: {}", e),
+            Ok((k, v)) => {
+                let key =
+                    std::string::String::from_utf8((&k[..]).to_vec()).unwrap_or(format!("{:?}", k));
+                let value = if v.is_ascii() {
+                    std::string::String::from_utf8((&v[..]).to_vec())
+                        .expect("I though this is ASCII?!")
+                } else {
+                    format!("{:?}", v)
+                };
+                println!("    \"{}\" => {}.", key, value,)
+            }
+        }
+    }
+
+    println!("\nKnown trees:");
+    for n in &db.tree_names() {
+        let n = std::string::String::from_utf8((&n[..]).to_vec()).unwrap_or(format!("{:?}", n));
+        if n.starts_with("__") {
+            continue;
+        }
+        println!("    \"{}\"", &n)
+    }
+    Ok(())
+}
